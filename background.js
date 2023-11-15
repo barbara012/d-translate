346a701ac7679d1183274a2f5170a9c33002169b
hwh <hwh@xkool.org>
2023-11-15 18:49:59 +0800

    添加谷歌翻译的支持,第一种方式失败后会自动调用谷歌翻译

--- background.js
+++ background.js
@@ -1,33 +1,89 @@
-// 监听
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.message === "translate") {
-      // 获取文本框的内容
-      const textToTranslate = request.text;
-
-      // 发送POST请求到翻译接口
-      fetch('https://api.deeplx.org/translate', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ text: textToTranslate, source_lang: 'auto', target_lang: request.targetLang }),
+const apiKey = 'AIzaSyBnEK4Eqp2XkWjSzit1J_rlcKPp0xXDM6Y'; // 替换为你的 API 密钥
+const apiUrl = 'https://translation.googleapis.com/language/translate/v2';
+const googleTrans = (text, targetLang) => {
+  return new Promise((resolve, reject) => {
+    fetch(`${apiUrl}?key=${apiKey}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        q: text,
+        target: targetLang
       })
-        .then(response => response.json())
-        .then(data => {
-          // 处理翻译结果，可以将结果发送回内容脚本
-          console.log('result', data);
-          // chrome.runtime.sendMessage({message: "translate", result: data })
-          sendResponse({message: "translate", result: data })
-        })
-        .catch(error => {
-          console.error('Error:', error);
-          // 处理错误，可以将错误发送回内容脚本
-          sendResponse(error)
-        });
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // 简单处理一下，同语言之间互译，可能会出现如下错误
+        if (
+          data.error &&
+          data.error.message &&
+          data.error.message.indexOf('Bad language pair') > -1
+        ) {
+          resolve({
+            data: text,
+            alternatives: []
+          });
+        } else {
+          resolve({
+            data: data.data.translations[0].translatedText,
+            alternatives: []
+          });
+        }
+      })
+      .catch(reject);
+  });
+};
 
-      // 返回 true，以保持 sendResponse 的有效性
-      return true;
-    }
+const deeplxTrans = (text, targetLang) => {
+  return new Promise((resolve, reject) => {
+    fetch('https://api.deeplx.org/translate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        text: text,
+        source_lang: 'auto',
+        target_lang: targetLang
+      })
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // 处理翻译结果，可以将结果发送回内容脚本
+        resolve(data);
+      })
+      .catch(reject);
+  });
+};
+
+// 监听
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === 'translate') {
+    // 获取文本框的内容
+    const textToTranslate = request.text;
+    deeplxTrans(textToTranslate, request.targetLang)
+      .then((data) => {
+        sendResponse({
+          message: 'translate',
+          result: data
+        });
+      })
+      .catch(() => {
+        googleTrans(textToTranslate, request.targetLang)
+          .then((data) => {
+            sendResponse({
+              message: 'translate',
+              result: data
+            });
+          })
+          .catch((error) => {
+            sendResponse({
+              message: 'translate',
+              error: error
+            });
+          });
+      });
+    return true;
   }
-);
+});
